5c09a107fe624f9368f16d33038fc8e3c2e99a0c
Mattia <75211269+mancarius@users.noreply.github.com>
2022-01-11 16:34:31 +0100

    added 'loading' and 'toast' states and related menagement

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,33 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    loading: false,
+    toast: null,
+  },
+  mutations: {
+    loading(state, payload) {
+      state.loading = payload;
+    },
+
+    toastOpen(state, payload) {
+      state.toast = payload;
+    },
+  },
+  actions: {
+    loadingStart() {
+      this.commit("loading", true);
+    },
+    loadingStop() {
+      this.commit("loading", false);
+    },
+    //
+    showError(store, message) {
+      this.commit("toastOpen", { message, type: "error" });
+    },
+    showInfo(store, message) {
+      this.commit("toastOpen", { message, type: "info" });
+    },
+  },
   modules: {},
 });
