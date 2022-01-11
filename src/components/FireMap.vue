1bd3afa7240e304fca688a850218f8eaf60b938b
Mattia <75211269+mancarius@users.noreply.github.com>
2022-01-11 16:32:04 +0100

    added data loading and datepicker

--- src/components/FireMap.vue
+++ src/components/FireMap.vue
@@ -1,29 +1,51 @@
 <template>
   <div class="map-container">
-    <l-map :zoom="zoom" :center="center">
+    <l-map :zoom="zoom" :center="center" @ready="isMapReady = true">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <l-marker
-        :lat-lng="markerLatLng"
-        v-for="markerLatLng in markersCoords"
-        :key="markerLatLng"
-      ></l-marker>
+      <l-control-scale
+        position="bottomleft"
+        :imperial="false"
+        :metric="true"
+      ></l-control-scale>
+      <l-control position="topright">
+        <datepicker-range v-model:selectedRange="date" />
+      </l-control>
+      <l-circle
+        :lat-lng="coords"
+        :radius="circle.radius"
+        :color="circle.color"
+        :fillColor="circle.fillColor"
+        :fillOpacity="circle.fillOpacity"
+        v-for="coords in markersCoords"
+        :key="coords"
+      ></l-circle>
     </l-map>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import { defineComponent, onMounted, Ref, ref, watch } from "vue";
+import {
+  LMap,
+  LTileLayer,
+  LCircle,
+  LControlScale,
+  LControl,
+} from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
+import DatepickerRange from "./DatepickerRange.vue";
+import FiresApiService from "@/services/FiresApiService";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "FireMap",
-  components: { LMap, LTileLayer, LMarker },
-  props: {
-    markersCoords: {
-      type: Array,
-      required: true,
-    },
+  components: {
+    LMap,
+    LTileLayer,
+    LCircle,
+    LControlScale,
+    LControl,
+    DatepickerRange,
   },
 
   data() {
@@ -33,6 +55,76 @@ export default defineComponent({
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       center: [47.31322, -1.319482],
       zoom: 1.5,
+      circle: {
+        radius: 1500,
+        color: "red",
+        fillColor: "red",
+        fillOpacity: 1,
+      },
+    };
+  },
+
+  setup() {
+    const date: Ref<[Date, Date] | null> = ref(null);
+    const markersCoords: Ref<[number, number][]> = ref([]);
+    const api = process.env.VUE_APP_API_URL;
+    const apiService = new FiresApiService(api);
+    const store = useStore();
+    const isMapReady = ref(false);
+    const isDataReady = ref(false);
+
+    const loadCoords = (dateRange: [Date, Date] | null) => {
+      if (dateRange) {
+        markersCoords.value = apiService
+          .filterByDateRange(...dateRange)
+          .map(({ latitude, longitude }) => [latitude, longitude]);
+      }
+    };
+
+    const checkCoordsLength = (coords: [number, number][]) => {
+      if (coords.length === 0) {
+        store.dispatch("showInfo", "No fires found in this periods");
+      }
+    };
+
+    watch(date, loadCoords);
+
+    watch(markersCoords, checkCoordsLength);
+
+    watch([isMapReady, isDataReady], ([mapReady, dataReady]) => {
+      if (mapReady && dataReady) {
+        store.dispatch("loadingStop");
+      } else {
+        store.dispatch("loadingStart");
+      }
+    });
+
+    onMounted(() => {
+      apiService
+        .load()
+        .then(() => {
+          markersCoords.value = apiService.data.map(
+            ({ latitude, longitude }) => [latitude, longitude]
+          );
+        })
+        .catch((error) => {
+          console.warn(error);
+          store.dispatch("showError", "Impossible to load fires data");
+        })
+        .finally(() => {
+          isDataReady.value = true;
+        });
+    });
+
+    store.dispatch("loadingStop");
+
+    return {
+      date,
+      markersCoords,
+      apiService,
+      store,
+
+      isMapReady,
     };
   },
 });
