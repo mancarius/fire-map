bc7ffe1a0705b7383c2a76c44feebe809f3980c3
Mattia <75211269+mancarius@users.noreply.github.com>
2022-01-11 16:36:19 +0100

    inserted fire-map component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <fire-map />
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/ThePageHeader.vue"; // @ is an alias to /src
+import FireMap from "@/components/FireMap.vue";
+import { defineComponent } from "vue";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+export default defineComponent({
+  components: { FireMap },
+});
 </script>
