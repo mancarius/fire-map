3d1bd3d1369623e145ebe08c1acfb0f50e4c8302
Mattia <75211269+mancarius@users.noreply.github.com>
2022-01-14 20:13:34 +0100

    added webpack configs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require("webpack");
+
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
   devServer: {
     https: true,
+  },
+  configureWebpack: {
+    plugins: [
+      // Define Bundler Build Feature Flags
+      new webpack.DefinePlugin({
+        // Drop Options API from bundle
+        __VUE_OPTIONS_API__: false,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+    ],
   },
 };
